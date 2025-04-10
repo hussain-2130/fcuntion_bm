b4813edde6eb0f10174f91394eb8a62ccfc9ca93
Ahammad-I <126706128+Ahammad-I@users.noreply.github.com>
2025-04-10 19:11:57 +0530

    TOTAL MATCH BUT NOT UNIQUE MATCHING

--- bookmark.py
+++ bookmark.py
@@ -1,4 +1,5 @@
 import fitz  # PyMuPDF
+import fitz  # PyMuPDF
 
 def add_highlight_annot(pdf_path, highlights):
     doc = fitz.open(pdf_path)
@@ -22,30 +23,15 @@ def add_highlight_annot(pdf_path, highlights):
             local_end = end - global_offset
 
             expected_text = page_text[local_start:local_end]
-            print(f"🧩 Trying to highlight '{expected_text}' ({start}-{end})")
+            print(f"🧩 Looking for: '{expected_text}' ({start}-{end})")
 
             matches = page.search_for(expected_text)
-            print(f"matches{matches}")
-            found_exact = False
-            for match in matches:
-           
-
-                for i in range(len(page_text) - len(expected_text) + 1):
-                    candidate = page_text[i:i+len(expected_text)]
-
-                    if candidate == expected_text and i == local_start:
-                        print(f"matched candidate{match}")
-
-                        highlight = page.add_highlight_annot(match)
-                        highlight.set_info(title=label)
-                        found_exact = True
-                        print(f"✅ Highlighted '{label}'")
-                        break
-                if found_exact:
-                    break
-
-            if not found_exact:
-                print(f"❌ Could not match exact position for: '{expected_text}'")
+            if matches:
+                for match in matches:
+                    page.add_highlight_annot(match).set_info(title=label)
+                print(f"✅ Highlighted '{label}' with {len(matches)} part(s)")
+            else:
+                print(f"❌ Could not find match for: '{expected_text}'")
 
         global_offset += page_length
 
@@ -55,14 +41,14 @@ def add_highlight_annot(pdf_path, highlights):
     print(f"\n✅ Highlighting complete. File saved at: {output_path}")
 
 
+   
+
 
 
 
 pdf_path = r"C:\Users\HP\Desktop\function_bm\12. Master Services Agreement.pdf"
 highlights = [
-    (0, 760,"zzzzzzzzzzzzzzz"),
-    (100, 110, "FACT"),
-    (300, 320, "POINT A")
+    (1000, 1760,"mumbaimumbai"),
 ]
 
 add_highlight_annot(pdf_path, highlights)
